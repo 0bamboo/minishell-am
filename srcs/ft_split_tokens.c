2540005c1909f5e6b578e267ef54878909304c70
paayk <itsabdennacerama@gmail.com>
2021-03-21 23:47:50 +0100

    command

--- srcs/ft_split_tokens.c
+++ srcs/ft_split_tokens.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                      :::      ::::::::   */
-/*   ft_split_tokens_tokens.c                                  :+:      :+:    :+:   */
+/*                                                       :::       ::::::::   */
+/*   ft_split_tokens.c                          		:+:       :+:    :+:  */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abdait-m <abdait-m@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,81 +12,6 @@
 
 #include "../includes/minishell.h"
 
-// static int			_count_tokens(char const *s, char c)
-// {
-// 	int	count;
-
-// 	count = 0;
-// 	while (*s && *s == c)
-// 		s++;
-// 	while (*s)
-// 	{
-// 		if ((*s == c && *(s + 1) != c) || *(s + 1) == '\0')
-// 			count++;
-// 		s++;
-// 	}
-// 	return (count);
-// }
-
-// static int			_len_tokens(char const *s, char c)
-// {
-// 	int	k;
-
-// 	k = 0;
-// 	while (s[k] && s[k] != c)
-// 		k++;
-// 	return (k);
-// }
-
-// static char			**_free(char **ptr, int size)
-// {
-// 	int i;
-
-// 	i = 0;
-// 	while (i++ < size)
-// 		free(ptr[i]);
-// 	free(ptr);
-// 	ptr = NULL;
-// 	return (NULL);
-// }
-
-// char		**ft_split_tokens(char const *s, char c)
-// {
-// 	char		**p;
-// 	int			size;
-// 	int			i;
-// 	int			j;
-
-// 	if (!s)
-// 		return (NULL);
-// 	size = _count_tokens(s, c);
-// 	if (!(p = (char **)malloc(sizeof(char*) * (size + 1))))
-// 		return (NULL);
-// 	i = 0;
-// 	while (i < size)
-// 	{
-// 		while (*s && *s == c)
-// 			s++;
-// 		if (!(p[i] = (char *)malloc(sizeof(char) * (_len_tokens(s, c) + 1))))
-// 			return (_free(p, size));
-// 		j = 0;
-// 		while (*s && *s != c)
-// 			p[i][j++] = *s++;
-// 		p[i++][j] = '\0';
-// 	}
-// 	p[i] = NULL;
-// 	return (p);
-// }
-
-// typedef struct 
-// {
-//     char **p;
-//     int size;
-//     int i;
-//     int j;
-//     int check_dq;
-//     int check_sq;
-// } s_split;
 
 static int			_count_tokens(char const *s, char c, s_split *sp)
 {
@@ -107,7 +32,6 @@ static int			_count_tokens(char const *s, char c, s_split *sp)
 	}
 	sp->check_sq = 0;
 	sp->check_dq = 0;
-    // printf("how many words : %d\n", count);
 	return (count);
 }
 
@@ -154,9 +78,6 @@ void _trim_tokens(char **tab)
 
 char				**_split_tokens(s_split *sp, char const *s, char c)
 {
-	// char **tab;
-	// int size;
-	
 	if (!s)
 		return (NULL);
 	sp->size = _count_tokens(s, c, sp);
@@ -166,8 +87,6 @@ char				**_split_tokens(s_split *sp, char const *s, char c)
 	{
 		while (*s && *s == c)
 			s++;
-		// So the problem is in _len_tokens : when you calculate each string length you stop when you find the the fist delimeter
-		// fixed maybe....
 		if (!(sp->p[sp->i] = (char *)malloc(sizeof(char) * (_len_tokens(sp, s, c) + 1))))
 			return (_free(sp));
 		sp->j = 0;
@@ -185,7 +104,6 @@ char				**_split_tokens(s_split *sp, char const *s, char c)
 	}
 	sp->p[sp->i] = NULL;
 	sp->i = 0;
-	// printf("split --> |%d|\n", sp->i);
 	_trim_tokens(sp->p);
 	return (sp->p);
 }
--- srcs/input_parser.c
+++ srcs/input_parser.c
@@ -6,17 +6,14 @@
 /*   By: abdait-m <abdait-m@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 18:38:31 by abdait-m          #+#    #+#             */
-/*   Updated: 2021/03/21 18:41:25 by abdait-m         ###   ########.fr       */
+/*   Updated: 2021/03/21 23:18:09 by abdait-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-//  Try to make a function that calculates the number of parenthesis and check if it is an error or not,
-// use open close method  : if " is open  then the next one should close it etcetera.....
 
-
-int count_parenthesis(char *line)
+int _check_parsing_errors(char *line)
 {
     int open_sq;
     int count_sq;
@@ -31,6 +28,8 @@ int count_parenthesis(char *line)
     count_dq = 0;
     while (line[++i])
     {
+        if (line[i] == ';' && line[i + 1] == ';' && open_dq == 0 && open_sq == 0)
+            return 1;
         if (line[i] == '"' && open_dq == 0 && open_sq == 0)
         {
             count_dq++;
@@ -55,96 +54,6 @@ int count_parenthesis(char *line)
     return (count_sq + count_dq);
 }
 
-
-// Try to use this split for your parsing :
-
-// typedef struct 
-// {
-//     char **p;
-//     int size;
-//     int i;
-//     int j;
-//     int check_pp;
-//     int check_p;
-// } s_split;
-
-// static int			ft_count_words(char const *s, char c, s_split *sp)
-// {
-// 	int	count;
-
-// 	count = 0;
-// 	while (*s && *s == c)
-// 		s++;
-// 	while (*s)
-// 	{
-//         if ('"' == *s && sp->check_p % 2 == 0)
-//             sp->check_pp += 1;
-//         if ('\'' == *s && sp->check_pp % 2 == 0)
-//             sp->check_p += 1;
-// 		if ((((*s == c && *(s + 1) != c) || *(s + 1) == '\0')) && sp->check_p % 2 == 0 && sp->check_pp % 2 == 0)
-// 			count++;
-// 		s++;
-// 	}
-//     printf("how many words : %d\n", count);
-// 	return (count);
-// }
-
-// static int			ft_len_words(char const *s, char c)
-// {
-// 	int	k;
-
-// 	k = 0;
-// 	while (s[k] && s[k] != c)
-// 		k++;
-// 	return (k);
-// }
-
-// static char			**ft_free(char **ptr, int size)
-// {
-// 	int i;
-
-// 	i = 0;
-// 	while (i++ < size)
-// 		free(ptr[i]);
-// 	free(ptr);
-// 	ptr = NULL;
-// 	return (NULL);
-// }
-
-// char				**_split_tokens(s_split *sp, char const *s, char c)
-// {
-// 	if (!s)
-// 		return (NULL);
-//     sp->check_pp = 0;
-//     sp->check_p = 0;
-// 	sp->size = ft_count_words(s, c, sp);
-// 	if (!(sp->p = (char **)malloc(sizeof(char*) * (sp->size + 1))))
-// 		return (NULL);
-// 	sp->i = 0;
-//     sp->check_pp = 0;
-//     sp->check_p = 0;
-// 	while (sp->i < sp->size)
-// 	{
-// 		while (*s && *s == c)
-// 			s++;
-// 		if (!(sp->p[sp->i] = (char *)malloc(sizeof(char) * (ft_len_words(s, c) + 1))))
-// 			return (ft_free(sp->p, sp->size));
-// 		sp->j = 0;
-// 		while (*s)
-//         {
-//             if (*s == '\'' && sp->check_pp % 2 == 0)
-//                 sp->check_p += 1;
-//             if (*s == '"' && sp->check_p % 2 == 0)
-//                 sp->check_pp += 1;
-//             sp->p[sp->i][sp->j++] = *s++;
-//             if (*s == c && sp->check_pp % 2 == 0 && sp->check_p % 2 == 0)
-//                 break;
-//         }
-// 		sp->p[sp->i++][sp->j] = '\0';
-// 	}
-// 	sp->p[sp->i] = NULL;
-// 	return (sp->p);
-// }
 
 void        raise_an_exception()
 {
@@ -172,39 +81,26 @@ void start_parsing(char *line, sc_parse *prs)
     int i;
     
     initialize_vars(&sp);
-    if (count_parenthesis(line))
+    if (_check_parsing_errors(line))
         raise_an_exception();
     else 
     {
         prs->sc_cmds = _split_tokens(&sp, line, ';');
         i = 0;
-        // printf("this the first string ---> |%s|\n", prs->sc_cmds[i]);
-        printf("--size------> %d\n", sp.size);
         prs->space_cmd = (char ***)malloc(sizeof(char **) * (sp.size));
         while (prs->sc_cmds[i])
         {
-            // Fix this one how to fill the table of commands and its options and arguments
             prs->space_cmd[i] = _split_tokens(&sp, prs->sc_cmds[i], ' ');
-            printf("|%s|--> |%s|\n", prs->sc_cmds[i], prs->space_cmd[i][0]);
             i++;
         }
         i = -1;
-        printf("--command--        --option--         -- args -->\n");
-        printf("--size------> %d\n", sp.size);
         while (prs->sc_cmds[++i])
         {
             j = -1;
             while (prs->space_cmd[i][++j])
-            {
                 printf("----|%s|----\t", prs->space_cmd[i][j]);
-                // printf("**%d**,\n", j);
-                // j++;
-            }
             printf("\n");
-            // printf("{%d}\n", i);
-            // i++;
         }
-        // write(1, "\n\nim here\n\n", 11);
         write(1, "free time\n", 10);
         // while (prs->space_cmd[i] != NULL)
         // {
